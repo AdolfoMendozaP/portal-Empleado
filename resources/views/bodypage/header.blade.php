6fec6df73395bc06dfb849cdddbaafac993b0098
Adolfo Mendoza <adolfitocraftero1225@gmail.com>
2024-10-07 15:35:03 -0600

    portal empleado 07-10-24

---
+++ resources/views/bodypage/header.blade.php
@@ -0,0 +1,60 @@
+<!-- Encabezado -->
+<div class="container-fluid bg-primary text-light py-3">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <h2 class="no-bold">Portal del Empleado</h2>
+        </div>
+        <div class="col-md-6 text-md-end"> 
+            <div class="d-flex align-items-center justify-content-end"> 
+                <div class="d-flex align-items-center ms-4"> 
+                <i class="fas fa-bell"></i>
+                <img src="https://w7.pngwing.com/pngs/981/645/png-transparent-default-profile-united-states-computer-icons-desktop-free-high-quality-person-icon-miscellaneous-silhouette-symbol-thumbnail.png" alt="" class="rounded-circle" width="30">
+                <span class="ms-2">John Doe</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tarjetas blancas -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card bg-white">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">PUESTO</h5>
+                        <p class="card-text"><strong>#</strong></p>
+                        <p class="card-text text-secondary">#</p>
+                    </div>
+                    <div class="card-icon">
+                        <i class="fas fa-id-card-alt text-danger"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-white">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">HORARIO</h5>
+                        <p class="card-text"><strong>#</strong></p>
+                        <p class="card-text text-secondary">Matutino</p>
+                    </div>
+                    <div class="card-icon">
+                        <i class="fas fa-clock text-orange"></i> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card bg-white">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary">PENDIENTES</h5>
+                        <p class="card-text"><strong>#</strong></p>
+                        <p class="card-text text-secondary">#</p>
+                    </div>
+                    <div class="card-icon">
+                        <i class="fas fa-file text-yellow"></i>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+   </div>
+ 
---
+++ resources/views/bodypage/menu.blade.php
@@ -0,0 +1,65 @@
+<ul class="menu">
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-briefcase fa-lg"></i>
+                    <span class="small-text">Portal</span>
+                </div>
+            </li>
+
+            <li class="active">
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-user-xmark fa-lg"></i>
+                    <span class="small-text">Ausencias y Retardos</span>
+                </div>
+            </li>
+
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-calendar-days fa-lg"></i> 
+                    <span class="small-text">Vacaciones</span>
+                </div>
+            </li>
+
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-book fa-lg"></i>
+                    <span class="small-text">Tareas</span>
+                </div>
+            </li>
+
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-users fa-lg"></i>
+                    <span class="small-text">Equipo</span>
+                </div>
+            </li>
+
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-chart-column fa-lg"></i>
+                    <span class="small-text">Estadisticas</span>
+                </div>
+            </li>
+
+            <li>
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-circle-user fa-lg"></i>
+                    <span class="small-text">Perfil</span>
+                </div>
+            </li>
+
+            <li class="logout">
+                <a href="#"></a>
+                <div class="menu-item">
+                    <i class="fa-solid fa-right-from-bracket fa-lg"></i>
+                    <span class="small-text">Cerrar Sesión</span>
+                </div>
+            </li>
+        </ul>
